3188de9422df9520f71f0832e42cde564a51f074
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-07-14 23:37:10 -0500

    local storage was added, time was working well with color code'

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Daily-Planner
+
+Accept Criteria
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+
+The following is a schedule that is color coded, based on the current time and whether it is in the past, present, or future. Any input should be save and be stored in local storage for future usage.
+
+
+
+![local storage](./assets/images/local%20storage.jpg)
+
+
+
+
+
+
+
+
+
+![Past, Present, Future](./assets/images/PPF.jpg)
+
+
+
+
+
+References:
+
+https://stackoverflow.com/questions/19304343/using-the-id-for-if-condition#:~:text=If%20you%27re%20trying%20to%20check%20the%20value%20of,item.attr%20%28%22id%22%29%3B%20if%20%28id%20%3D%3D%20%27bar%27%29%20%7B%20%7D
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -3,115 +3,7 @@ $('#currentDay').text(time);
 
 
 function doubleStack() {
-    
-    // old method, wasn't reading as am/pm, and was just reading the value of the textarea as a number.
-
-// var times9 = $('.hour9').text()
-// var times10 = $('.hour10').text()
-// var times11 = $('.hour11').text()
-// var times12 = $('.hour12').text()
-// var times13 = $('.hour13').text()
-// var times14 = $('.hour14').text()
-// var times15 = $('.hour15').text()
-// var times16 = $('.hour16').text()
-// var times17 = $('.hour17').text()
-// console.log(times)
-
-
-// var firstNumber = parseInt(times9)
-// var secondNumber = parseInt(times10)
-// var thirdNumber = parseInt(times11)
-// var fourthNumber = parseInt(times12)
-// var fifthNumber = parseInt(times13)
-// var sixNumber = parseInt(times14)
-// var sevenNumber = parseInt(times15)
-// var eightNumber = parseInt(times16)
-// var nineNumber = parseInt(times17)
-
-// console.log(divs)
-    // var blocks = $("div").attr('id')
-    // console.log(blocks)
-
-
-// if(currentTime > blocks) {
-//     $('#hr9').addClass('past') 
-// } else if(currentTime == firstNumber) {
-//     $('#hr9').addClass('present')
-// } else if(currentTime < firstNumber) {
-//     $('#hr9').addClass('future')
-// }
-
-
-// if(currentTime > secondNumber) {
-//     $('#hr10').addClass('past')
-// } else if(currentTime == secondNumber) {
-//     $('#hr10').addClass('present')
-// } else if(currentTime < secondNumber) {
-//     $('#hr10').addClass('future')
-// }
-
-
-// if(currentTime > thirdNumber) {
-//     $('#hr11').addClass('past')
-// } else if(currentTime == thirdNumber) {
-//     $('#hr11').addClass('present')
-// } else if(currentTime < thirdNumber) {
-//     $('#hr11').addClass('future')
-// }
-
-
-// if(currentTime > fourthNumber) {
-//     $('#hr12').addClass('past')
-// } else if(currentTime == fourthNumber) {
-//     $('#hr12').addClass('present')
-// } else if(currentTime < fourthNumber) {
-//     $('#hr12').addClass('future')
-// }
-
-
-// if(currentTime > fifthNumber) {
-//     $('#hr13').addClass('past')
-// } else if(currentTime == fifthNumber) {
-//     $('#hr13').addClass('present')
-// } else if(currentTime < fifthNumber) {
-//     $('#hr13').addClass('future')
-// }
-
-// if(currentTime > sixNumber) {
-//     $('#hr14').addClass('past')
-// } else if(currentTime == sixNumber) {
-//     $('#hr14').addClass('present')
-// } else if(currentTime < sixNumber) {
-//     $('#hr14').addClass('future')
-// }
-
-
-// if(currentTime > sevenNumber) {
-//     $('#hr15').addClass('past')
-// } else if(currentTime == sevenNumber) {
-//     $('#hr15').addClass('present')
-// } else if(currentTime < sevenNumber) {
-//     $('#hr15').addClass('future')
-// }
-
-// if(currentTime > eightNumber) {
-//     $('#hr16').addClass('past')
-// } else if(currentTime == eightNumber) {
-//     $('#hr16').addClass('present')
-// } else if(currentTime < eightNumber) {
-//     $('#hr16').addClass('future')
-// }
-
-// if(currentTime > nineNumber) {
-//     $('#hr17').addClass('past')
-// } else if(currentTime == nineNumber) {
-//     $('#hr17').addClass('present')
-// } else if(currentTime < nineNumber) {
-//     $('#hr17').addClass('future')
-// }
-
-
-
+  
 
 var currentTime = moment().format('H')
     // console.log(currentTime)
@@ -120,14 +12,13 @@ var divs = $('div')
 
 divs.each(function(index, value){
     var id = $(value).attr('id')
-    console.log(id)
+    // console.log(id)
 
-    if(currentTime === id) {
-        $(value).next().addClass('present')
-        
-    } else if(currentTime > id) {
+    if(currentTime > id) {
         $(value).next().addClass("past")
-    } else if(currentTime < id) {
+    } else if(currentTime === id) {
+        $(value).next().addClass('present')        
+    }  else if(currentTime < id) {
         $(value).next().addClass('future')
 
     }
@@ -139,34 +30,97 @@ divs.each(function(index, value){
 
 doubleStack()
 
-// Local Storage
+$(document).ready(function(){ 
 
-    var getInfo = $('.information')
-    var getTime = $('.hour9')
+$('.saveInput').on('click', function(){
+    var text = $('#test').val()
 
-// function getValue() {
-//     var info = getInfo.value
-//     var tiempo = getTime.value
-
-//     localStorage.setItem(tiempo, info)
-
-// }
-
-$('.button').on('click', function() {
-    var info = getInfo.value
-    var tiempo = getTime.value
-
-    localStorage.setItem(tiempo, info)
-
+    localStorage.setItem('Info', text)
+})
 
 })
 
-// $('.button').on('click' function() {
+$(document).ready(function(){ 
+
+    $('.saveInput1').on('click', function(){
+        var text = $('#test1').val()
     
-//     var getInfo = $('.information').value
-//     var getTime = $('.hour9').value
-// })
+        localStorage.setItem('Info', text)
+    })
+    
+    })
+
+    $(document).ready(function(){ 
+
+        $('.saveInput2').on('click', function(){
+            var text = $('#test2').val()
+        
+            localStorage.setItem('Info', text)
+        })
+        
+        })
+
+        $(document).ready(function(){ 
+
+            $('.saveInput3').on('click', function(){
+                var text = $('#test3').val()
+            
+                localStorage.setItem('Info', text)
+            })
+            
+            })
+
+            $(document).ready(function(){ 
+
+                $('.saveInput4').on('click', function(){
+                    var text = $('#test4').val()
+                
+                    localStorage.setItem('Info', text)
+                })
+                
+                })
+
+                $(document).ready(function(){ 
+
+                    $('.saveInput5').on('click', function(){
+                        var text = $('#test5').val()
+                    
+                        localStorage.setItem('Info', text)
+                    })
+                    
+                    })
+
+         $(document).ready(function(){ 
+
+          $('.saveInput6').on('click', function(){
+              var text = $('#test6').val()
+                        
+               localStorage.setItem('Info', text)
+           })
+                        
+     })
+
+         $(document).ready(function(){ 
+
+             $('.saveInput7').on('click', function(){
+                  var text = $('#test7').val()
+                    
+               localStorage.setItem('Info', text)
+             })
+                            
+         })
+
+         $(document).ready(function(){ 
+
+                 $('.saveInput8').on('click', function(){
+                       var text = $('#test8').val()
+                                
+                    localStorage.setItem('Info', text)
+             })
+                                
+         })
 
 
-
-
+        //  retrieve local storage Info
+//    var noRefresh = localStorage.getItem(text)
+         
